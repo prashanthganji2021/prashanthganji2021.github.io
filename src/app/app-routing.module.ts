980f7f30ef852fa886bd893690c13bc4a570aa66
PROJECTS <111948535+prashanthganji2021@users.noreply.github.com>
2023-03-08 21:03:49 +0000

    feat: frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserDetailsComponent } from './user/user-details/user-details.component';
-import { UserCreateComponent } from './user/user-create/user-create.component';
-import { UserUpdateComponent } from './user/user-update/user-update.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './auth.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserCreateComponent } from './user/user-create/user-create.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserUpdateComponent } from './user/user-update/user-update.component';
-import { UserDetailsComponent } from './user/user-details/user-details.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import {FileUploadModule} from "ng2-file-upload";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeaderComponent } from './header/header.component';  
-import {MatCardModule} from '@angular/material/card';
-import { DataTablesModule } from 'angular-datatables';
-import { MatTooltipModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserListComponent,
-    UserCreateComponent,
-    UserUpdateComponent,
-    UserDetailsComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    FileUploadModule,
-    MatCardModule,
-    DataTablesModule,
-    MatTooltipModule,
-    BrowserAnimationsModule,
-    ModalModule.forRoot(),
-
+    AppRoutingModule
   ],
-  providers: [BsModalRef],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
